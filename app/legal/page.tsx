07566267f4a84c63c2e8f3ce2ec1836573793c96
aldjazoulymohammed96-afk <aldjazoulymohammed96@gmail.com>
2026-03-02 10:18:30 +0000

    Create legal page with terms of service and privacy policy

---
+++ app/legal/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LegalPage = () => {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+      <p>Your terms of service will go here.</p>
+
+      <h1>Privacy Policy</h1>
+      <p>Your privacy policy will go here.</p>
+    </div>
+  );
+};
+
+export default LegalPage;
