1665a2f858f4b05bc0866a1fdc02ae86a1b3cb8a
aldjazoulymohammed96-afk <aldjazoulymohammed96@gmail.com>
2026-03-02 10:09:40 +0000

    Create home page file

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+// This is the home page for the Black Cat application.
+
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Black Cat!</h1>
+            <p>Your home page content goes here.</p>
+        </div>
+    );
+};
+
+export default HomePage;
